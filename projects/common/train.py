469c9ae687ae81d34487db87cf8d30a424fb65f6
wilka <wcarvalho92@gmail.com>
2022-04-10 20:41:15 -0400

    created named tuple for network and also create observers now

--- projects/common/train.py
+++ projects/common/train.py
@@ -1,12 +1,62 @@
 from absl import app
 from absl import flags
+import collections
 import acme
+from acme.jax import utils
+
 import functools
+import jax
 
 from agents import td_agent
 from utils import make_logger, gen_log_dir
 
-from projects.common.environment_loop import EnvironmentLoop
+from projects.common.observers import LevelReturnObserver
+
+from acme.environment_loop import EnvironmentLoop
+
+def create_net_prediction_tuple(config, env_spec, NetworkCls, NetKwargs, data_length = 2):
+  """ Create dummy output and use that to define a NamedTuple that will be used to collect data. 
+  Note: Assumes BasicArch will be used.
+  
+  Args:
+      config (TYPE): Description
+      env_spec (TYPE): Description
+      NetworkCls (TYPE): Description
+      NetKwargs (TYPE): Description
+      data_length (int, optional): Description
+  
+  Returns:
+      TYPE: Description
+  """
+  # -----------------------
+  # preparate data
+  # -----------------------
+  dummy_obs = utils.tile_nested(
+      utils.zeros_like(env_spec.observations), config.batch_size)
+  dummy_obs_se = utils.tile_nested(dummy_obs, data_length)
+
+  # -----------------------
+  # prepare network
+  # -----------------------
+  dummy_networks = td_agent.make_networks(
+        batch_size=config.batch_size,
+        env_spec=env_spec,
+        NetworkCls=NetworkCls,
+        NetKwargs=NetKwargs,
+        eval_network=True)
+  dummy_key = jax.random.PRNGKey(42)
+  dummy_params = dummy_networks.init(dummy_key)
+  dummy_state = dummy_networks.initial_state.apply(
+    dummy_params, dummy_key, config.batch_size)
+  dummy_output, _ = dummy_networks.unroll.apply(
+    dummy_params,
+    dummy_key,
+    dummy_obs_se,
+    dummy_state,
+    dummy_key)
+
+  keys = dummy_output._asdict().keys()
+  return collections.namedtuple('Predictions', keys, defaults=(None,) * len(keys))
 
 def run(env,
     env_spec,
@@ -19,24 +69,37 @@ def run(env,
     log_dir: str,
     evaluate: bool=False,
     seed: int=1,
-    num_episodes: int=1_000):
+    num_episodes: int=1_000,
+    observers=None):
 
-
+  # -----------------------
+  # loggers + observers
+  # -----------------------
   logger_fn = lambda : make_logger(
     wandb=False,
     log_dir=log_dir,
     label=loss_label)
 
+  observers = observers or [LevelReturnObserver()]
   # -----------------------
   # agent
   # -----------------------
   builder=functools.partial(td_agent.TDBuilder,
-      LossFn=LossFn, LossFnKwargs=LossFnKwargs,
-      logger_fn=logger_fn)
+      LossFn=LossFn,
+      LossFnKwargs=LossFnKwargs,
+      logger_fn=logger_fn,
+      learner_kwargs=dict(clear_sgd_cache_period=config.clear_sgd_cache_period)
+      )
 
   kwargs={}
   if evaluate:
     kwargs['behavior_policy_constructor'] = functools.partial(td_agent.make_behavior_policy, evaluation=True)
+
+  # -----------------------
+  # prepare networks
+  # -----------------------
+  PredCls = create_net_prediction_tuple(config, env_spec, NetworkCls, NetKwargs)
+  NetKwargs.update(PredCls=PredCls)
 
   agent = td_agent.TDAgent(
       env_spec,
@@ -62,6 +125,10 @@ def run(env,
     label='actor',
     steps_key="steps")
 
-  loop = EnvironmentLoop(env, agent, logger=env_logger)
+  loop = EnvironmentLoop(
+    env,
+    agent,
+    logger=env_logger,
+    observers=observers or ())
   loop.run(num_episodes)
 
