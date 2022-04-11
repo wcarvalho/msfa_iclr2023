121a5d300c9cdce96b5d8dd34a273244401ccdb7
wilka <wcarvalho92@gmail.com>
2022-04-10 20:53:47 -0400

    {added (1) observers (2) cache sgd config arg (3) preloading network namedtuple}

--- projects/common/train_distributed.py
+++ projects/common/train_distributed.py
@@ -7,13 +7,15 @@ from absl import app
 from absl import flags
 import acme
 from acme.utils import paths
+from acme.jax.layouts import distributed_layout
 import functools
 
 from agents import td_agent
 from utils import make_logger, gen_log_dir
 from utils import data as data_utils
-
-from projects.common.environment_loop import EnvironmentLoop
+# from projects.common.environment_loop import EnvironmentLoop
+from projects.common.observers import LevelReturnObserver
+from projects.common.train import create_net_prediction_tuple
 
 def build_common_program(
   environment_factory,
@@ -29,7 +31,15 @@ def build_common_program(
   loss_label,
   wandb_init_kwargs=None,
   log_every=30.0, # how often to log
+  colocate_learner_replay=True,
+  observers=None,
   ):
+
+  # -----------------------
+  # prepare networks
+  # -----------------------
+  PredCls = create_net_prediction_tuple(config, env_spec, NetworkCls, NetKwargs)
+  NetKwargs.update(PredCls=PredCls)
 
   def network_factory(spec):
     return td_agent.make_networks(
@@ -40,31 +50,37 @@ def build_common_program(
       eval_network=True)
 
   builder=functools.partial(td_agent.TDBuilder,
-      LossFn=LossFn, LossFnKwargs=LossFnKwargs,
+      LossFn=LossFn,
+      LossFnKwargs=LossFnKwargs,
+      learner_kwargs=dict(clear_sgd_cache_period=config.clear_sgd_cache_period),
     )
 
   # -----------------------
-  # loggers
+  # loggers + observers
   # -----------------------
   use_wandb = True if wandb_init_kwargs is not None else False
   logger_fn = lambda : make_logger(
         log_dir=log_dir,
         label=loss_label,
+        time_delta=log_every,
         wandb=use_wandb,
         asynchronous=True)
 
   actor_logger_fn = lambda actor_id: make_logger(
                   log_dir=log_dir, label='actor',
+                  time_delta=log_every,
                   wandb=use_wandb,
                   save_data=actor_id == 0,
                   steps_key="actor_steps",
                   )
-  evaluator_logger_fn = lambda : make_logger(
+  evaluator_logger_fn = lambda label, steps_key: make_logger(
                   log_dir=log_dir, label='evaluator',
+                  time_delta=log_every,
                   wandb=use_wandb,
                   steps_key="evaluator_steps",
                   )
 
+  observers = observers or [LevelReturnObserver()]
   # -----------------------
   # wandb setup
   # -----------------------
@@ -99,9 +115,14 @@ def build_common_program(
     file=config_path,
     dictionary=save_config_dict)
 
+  ckpt_config= distributed_layout.CheckpointingConfig(
+    directory=log_dir)
+
   # -----------------------
   # build program
   # -----------------------
+  print("multithreading_colocate_learner_and_reverb", multithreading_colocate_learner_and_reverb)
+  import ipdb; ipdb.set_trace()
   return td_agent.DistributedTDAgent(
       environment_factory=environment_factory,
       environment_spec=env_spec,
@@ -110,10 +131,12 @@ def build_common_program(
       logger_fn=logger_fn,
       actor_logger_fn=actor_logger_fn,
       evaluator_logger_fn=evaluator_logger_fn,
-      EnvLoopCls=EnvironmentLoop,
+      # EnvLoopCls=EnvironmentLoop,
       config=config,
-      # workdir=log_dir,
+      checkpointing_config=ckpt_config,
       seed=config.seed,
       num_actors=num_actors,
       max_number_of_steps=config.max_number_of_steps,
-      log_every=log_every).build()
+      log_every=log_every,
+      observers=observers,
+      multithreading_colocate_learner_and_reverb=colocate_learner_replay).build()
