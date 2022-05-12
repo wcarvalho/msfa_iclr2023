09cea432b025ecfce5b4f1225472bf12b55e9d1b
Wilka <wcarvalho92@gmail.com>
2022-05-12 18:57:39 -0400

    minor

--- projects/kitchen_gridworld/configs.py
+++ projects/kitchen_gridworld/configs.py
@@ -115,7 +115,7 @@ class FarmConfig:
 
   # Network hps
   module_size: int = 128
-  nmodules: int = 6
+  nmodules: int = 4
   out_layers: int = 0
   module_attn_size: int = None
   module_attn_heads: int = 2  # how many attention heads between modules
