14d0f47c87b495a5137ac57af50ef38bb136b5ab
Wilka T Carvalho <wcarvalh@rldl13.eecs.umich.edu>
2022-05-23 19:01:47 -0400

    updates

--- projects/kitchen_gridworld/experiments.sh
+++ projects/kitchen_gridworld/experiments.sh
@@ -7,8 +7,9 @@ make train_search group=reward5 search=reward_msf cuda=0,1,2,3
 make train_search group=phi3 search=phi cuda=0,1,2,3
 make train_search group=gen4 search=gen cuda=0,1,2,4 cpus=5
 make train_search group=reward_usfa search=reward_usfa cuda=0,1,2,3
+make train_search group=mods2 search=mods2 cuda=0,1,2,4
 
 
 make train_search group=cov3 search=cov3 cuda=0,1,2,4
-make train_search group=mods2 search=mods2 cuda=0,1,2,4
 make train_search group=genv3 search=genv3 cuda=0,1,2,3
+make train_search group=phi2 search=phi2 cuda=0,1,2,4
