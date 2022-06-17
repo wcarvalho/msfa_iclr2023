7c67e6a5299626836f44953f5e5bf1069d1114cb
Wilka Carvalho <wcarvalho92@gmail.com>
2022-06-16 21:31:38 -0400

    minor

--- projects/kitchen_combo/configs.py
+++ projects/kitchen_combo/configs.py
@@ -6,17 +6,14 @@ from projects.kitchen_gridworld import configs
 class R2D1Config(configs.R2D1Config):
   task_embedding: str='none'
   embed_task_dim: int=6
-  out_hidden_size: int=512
 
 @dataclasses.dataclass
 class USFAConfig(configs.USFAConfig, R2D1Config):
   reward_coeff: float = 10
-  out_hidden_size: int=512
 
 @dataclasses.dataclass
 class ModularUSFAConfig(configs.ModularUSFAConfig, USFAConfig):
   reward_coeff: float = 10
-  out_hidden_size: int=512
 
 @dataclasses.dataclass
 class FarmConfig(configs.FarmConfig):
