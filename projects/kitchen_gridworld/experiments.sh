33e5e4292480d90dae043503557bf2d2d11fb635
wilka carvalho <wcarvalho92@gmail.com>
2022-06-30 17:27:02 -0400

    updates

--- projects/kitchen_gridworld/experiments.sh
+++ projects/kitchen_gridworld/experiments.sh
@@ -9,8 +9,6 @@ make lp_search search=test12_baselines cuda=0,1,2,3 agent=r2d1
 make meta_search searches=long_baselines cuda=0,1,2,3
 make meta_search searches=long_msf cuda=0,1,2,3
 
-<<<<<<< HEAD
+
 make meta_search agent=msf searches='test9_targets' cuda=0,1,2,4 
-=======
 make meta_search agent=msf searches='test8' cuda=0,1,2,3,4,5,6,7 terminal='current_terminal' agent=genv5
->>>>>>> c22f4c14503048d361c97dae617a304f0b884682
